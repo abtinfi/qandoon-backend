22e45757ab2b1f2dbde756a257a676cf2509a62b
Amir <amir.mzadeh81@gmail.com>
2025-04-23 23:25:38 +0330

    add otp model

---
+++ backend/models/otp.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, String, DateTime, Boolean, Integer, Enum
+from sqlalchemy.sql import func
+from backend.database.config import Base
+from datetime import datetime, timezone
+import enum
+
+class OTPPurpose(enum.Enum):
+    REGISTRATION = "registration"
+    PASSWORD_RESET = "password_reset"
+
+class OTP(Base):
+    __tablename__ = "otps"
+
+    id = Column(String, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    code = Column(String)
+    is_verified = Column(Boolean, default=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    expires_at = Column(DateTime(timezone=True))
+    attempts = Column(Integer, default=0)
+    purpose = Column(Enum(OTPPurpose), nullable=False)
+
+    def is_expired(self):
+        return datetime.now(timezone.utc) > self.expires_at
+
+    def increment_attempts(self):
+        self.attempts += 1 
